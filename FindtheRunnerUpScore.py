4cc280d9991fe1ce7d4656d14b021bb64f5e0b34
Guilherme Dias <80716667+Siluryan@users.noreply.github.com>
2021-12-23 16:55:44 -0300

    Create FindtheRunnerUpScore.py

---
+++ FindtheRunnerUpScore.py
@@ -0,0 +1,19 @@
+'''
+Given the participants' score sheet for your University Sports Day, you are required to find the runner-up score.
+You are given (n) scores. 
+Store them in a list and find the score of the runner-up.
+
+Input Format
+
+The first line contains (n). The second line contains an array A [] of (n) integers each separated by a space.
+'''
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = map(int, input().split())
+
+    A = list(set(arr))
+    M = max(A)
+    A.remove(M)
+
+print(max(A))
