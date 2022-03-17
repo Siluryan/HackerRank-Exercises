46228c1622a60ad5b97a039fdc5c2b078ea9dedc
Guilherme Dias <80716667+Siluryan@users.noreply.github.com>
2022-03-16 21:05:17 -0300

    Create Itertools_product.py

---
+++ Itertools_product.py
@@ -0,0 +1,18 @@
+
+'''
+Sample Input:
+1 2
+3 4
+Sample Output:
+(1, 3) (1, 4) (2, 3) (2, 4)
+'''
+
+a = input().split()
+b = input().split()
+c = [(int(x), int(y)) for x in a for y in b]
+d = str()
+
+for i in c:
+    d += str(i) + ' '
+
+print(d)
