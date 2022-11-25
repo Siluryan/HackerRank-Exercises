ec19dc389f9be16768dc538d5dd3e90471f02744
Guilherme Dias <guilhermerrdias@gmail.com>
2022-11-25 05:28:59 -0300

    training-1/12

---
+++ Interviews/MinMax.py
@@ -0,0 +1,37 @@
+# Given five positive integers, find the minimum and maximum values
+# that can be calculated by summing exactly four of the five integers.
+# Then print the respective minimum and maximum values as a single line
+# of two space-separated long integers.
+
+# !/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+#
+# Complete the 'miniMaxSum' function below.
+#
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def miniMaxSum(arr):
+    min_sum = 0
+    max_sum = 0
+    arr.sort()
+    res = arr[::-1]
+
+    for i in range(4):
+        min_sum += arr[i]
+        max_sum += res[i]
+
+    print(min_sum, max_sum)
+
+
+if __name__ == '__main__':
+    arr = list(map(int, input().rstrip().split()))
+
+    miniMaxSum(arr)
---
+++ Interviews/PlusMinus.py
@@ -0,0 +1,43 @@
+# Given an array of integers, calculate the ratios of its elements that are positive,
+# negative, and zero. Print the decimal value of each fraction on a new line with
+# 6 places after the decimal.
+
+
+#!/bin/python3
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'plusMinus' function below.
+#
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+positive = 0
+negative = 0
+zero = 0
+
+
+def plusMinus(arr, positive, negative, zero):
+    for i in arr:
+        if i < 0:
+            negative += 1
+        elif i == 0:
+            zero += 1
+        else:
+            positive += 1
+
+    positive = positive / n
+    negative = negative / n
+    zero = zero / n
+
+    print('%.6f\n %.6f\n %.6f' % (positive, negative, zero))
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    arr = list(map(int, input().rstrip().split()))
+
+    plusMinus(arr, positive, negative, zero)
