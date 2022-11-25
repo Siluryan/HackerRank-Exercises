ff4d04f7e1b037530a803e240f2745f6eb23e131
Guilherme Dias <guilhermerrdias@gmail.com>
2022-11-25 13:39:57 -0300

    breakintherecords

--- Interviews/PlusMinus.py
+++ Interviews/PlusMinus.py
@@ -16,7 +16,11 @@ import sys
 # The function accepts INTEGER_ARRAY arr as parameter.
 #
 
-def plusMinus(arr, positive, negative, zero):
+def plusMinus(arr):
+    positive = 0
+    negative = 0
+    zero = 0
+    
     for i in arr:
         if i < 0:
             negative += 1
@@ -34,8 +38,6 @@ def plusMinus(arr, positive, negative, zero):
 if __name__ == '__main__':
     n = int(input().strip())
     arr = list(map(int, input().rstrip().split()))
-    positive = 0
-    negative = 0
-    zero = 0
 
-    plusMinus(arr, positive, negative, zero)
+
+    plusMinus(arr)
