0a7b3da51488e5896a079179fecc709b5afbe242
Guilherme Dias <guilhermerrdias@gmail.com>
2022-11-25 20:16:28 -0300

    camelcase

--- Interviews/CamelCase.py
+++ Interviews/CamelCase.py
@@ -1,7 +1,7 @@
 # Input Format
 
 # Each line of the input file will begin with an operation (S or C) followed by a semi-colon followed
-# by #M, C, or V followed by a semi-colon followed by the words you'll need to operate on.
+# by M, C, or V followed by a semi-colon followed by the words you'll need to operate on.
 # The operation will either be S (split) or C (combine)
 # M indicates method, C indicates class, and V indicates variable
 # In the case of a split operation, the words will be a camel case method, class or variable name that # you need to split into a space-delimited list of words starting with a lowercase letter.
