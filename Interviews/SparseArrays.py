1dae96035cc0f28f8d8cc219ed91ffad3c95c4eb
Guilherme Dias <guilhermerrdias@gmail.com>
2022-11-27 19:09:51 -0300

    sparsearrays

---
+++ Interviews/SparseArrays.py
@@ -0,0 +1,115 @@
+'''There is a collection of input strings and a collection of query strings.
+For each query string, determine how many times it occurs in the list of input strings.
+Return an array of the results.'''
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'matchingStrings' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. STRING_ARRAY strings
+#  2. STRING_ARRAY queries
+#
+
+def matchingStrings(strings, queries):
+    dic = dict()
+    count = int()
+    result = list()
+    for x in queries:
+        dic [x] = 0
+        count = 0
+        for y in strings:               
+            if x == y:
+                dic[x] = count + 1
+                count += 1
+
+    for i in dic:
+        result.append(dic[i])
+
+    return(result)    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    strings_count = int(input().strip())
+
+    strings = []
+
+    for _ in range(strings_count):
+        strings_item = input()
+        strings.append(strings_item)
+
+    queries_count = int(input().strip())
+
+    queries = []
+
+    for _ in range(queries_count):
+        queries_item = input()
+        queries.append(queries_item)
+
+    res = matchingStrings(strings, queries)
+
+    fptr.write('\n'.join(map(str, res)))
+    fptr.write('\n')
+
+    fptr.close()
+
+# The best solution:
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'matchingStrings' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. STRING_ARRAY strings
+#  2. STRING_ARRAY queries
+#
+
+def matchingStrings(strings, queries):
+    d = {}
+    for string in strings:
+        d[string] = d.get(string, 0) + 1
+
+    return [d.get(query, 0) for query in queries]
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    strings_count = int(input().strip())
+
+    strings = []
+
+    for _ in range(strings_count):
+        strings_item = input()
+        strings.append(strings_item)
+
+    queries_count = int(input().strip())
+
+    queries = []
+
+    for _ in range(queries_count):
+        queries_item = input()
+        queries.append(queries_item)
+
+    res = matchingStrings(strings, queries)
+
+    fptr.write('\n'.join(map(str, res)))
+    fptr.write('\n')
+
+    fptr.close()
