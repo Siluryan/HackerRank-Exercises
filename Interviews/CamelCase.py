9311b728bf0aa09bf9e8415bdfa8482c736d8286
Guilherme Dias <guilhermerrdias@gmail.com>
2022-11-27 15:53:53 -0300

    divisiblesumpairs+ajuste

--- Interviews/CamelCase.py
+++ Interviews/CamelCase.py
@@ -1,21 +1,21 @@
-# Input Format
+''' Input Format
 
-# Each line of the input file will begin with an operation (S or C) followed by a semi-colon followed
-# by M, C, or V followed by a semi-colon followed by the words you'll need to operate on.
-# The operation will either be S (split) or C (combine)
-# M indicates method, C indicates class, and V indicates variable
-# In the case of a split operation, the words will be a camel case method, class or variable name that # you need to split into a space-delimited list of words starting with a lowercase letter.
-# In the case of a combine operation, the words will be a space-delimited list of words starting with
-# lowercase letters that you need to combine into the appropriate camel case String.
-# Methods should end #with an empty set of parentheses to differentiate them from variable names.
+Each line of the input file will begin with an operation (S or C) followed by a semi-colon followed
+by M, C, or V followed by a semi-colon followed by the words you'll need to operate on.
+The operation will either be S (split) or C (combine)
+M indicates method, C indicates class, and V indicates variable
+In the case of a split operation, the words will be a camel case method, class or variable name that # you need to split into a space-delimited list of words starting with a lowercase letter.
+In the case of a combine operation, the words will be a space-delimited list of words starting with
+lowercase letters that you need to combine into the appropriate camel case String.
+Methods should end with an empty set of parentheses to differentiate them from variable names.
 
-# Output Format
+Output Format
 
-# For each input line, your program should print either the space-delimited list of words (in the case
-# of a split operation) or the appropriate camel case string (in the case of a combine operation).
+For each input line, your program should print either the space-delimited list of words (in the case
+of a split operation) or the appropriate camel case string (in the case of a combine operation).
 
 
-# Enter your code here. Read input from STDIN. Print output to STDOUT
+Enter your code here. Read input from STDIN. Print output to STDOUT'''
 
 while True:
     try:     
---
+++ Interviews/DivisibleSumPairs.py
@@ -0,0 +1,51 @@
+'''Given an array of integers and a positive integer k, determine the number of (i,j) pairs
+where i < j and ar[i] + ar[j] is divisible by k.'''
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'divisibleSumPairs' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER k
+#  3. INTEGER_ARRAY ar
+#
+
+def divisibleSumPairs(n, k, ar):
+    z = 1
+    count = 0
+    for i in range(n-1):
+        x = ar[i]        
+        for u in range(i+1,n):
+            y = ar[u]        
+            if (x+y)%k == 0:                        
+                count +=1    
+        z += 1   
+                
+    return(count)              
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    k = int(first_multiple_input[1])
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = divisibleSumPairs(n, k, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
